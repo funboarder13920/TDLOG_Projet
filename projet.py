3b57cee0dcc8cdd8db5f72099512dac58cb1bea9
valentin.berkes <funboarder13920@free.fr>
2015-12-06 16:18:23 +0100

    Ajout log debut + initialisation complete

--- projet.py
+++ projet.py
@@ -1,30 +1,92 @@
+import random
+import logging
+import logging.config
+
+
+logging.config.fileConfig("log.conf")
+log = logging.getLogger("application")
 nbColonne = 13
 nbLigne = 8
+prop = ["ordinaire","ordinaire","dur","costaud","fute","rapide"]
+
+def intInput(str):
+	while True:
+		try:
+			num1 = int(input(str))
+		except ValueError:
+			continue
+		else:
+			break
+	return num1
+
+def choixPos(nEquipe):
+	log.debug("choixPos sur nEquipe = %d",nEquipe)
+	if not (nEquipe == 2 or nEquipe == 1):
+		log.error("Erreur dans choixPos avec nEquipe = %d",nEquipe)
+		assert nEquipe == 2 or nEquipe == 1
+	positions = []
+	k = 0
+	while k < 6 :
+		print(prop[k],"?")
+		posx = intInput("x: ")
+		posy = intInput("y: ")
+		log.info("ChoixPos : input (%d,%d)",posx,posy)
+		if (nEquipe == 1):
+			if (posx>= 1 and posx <=2 and posy>=0 and posy < 8):
+				if not (posx,posy) in positions:
+					log.info("ChoixPos: append de (%d,%d)",posx,posy)
+					k+=1
+					positions.append((posx,posy))
+				else:
+					log.info("ChoixPos: position déjà occupée")
+			else:
+				log.info("ChoixPos : (%d,%d) hors limite",posx,posy)
+		else :
+			if (posx>= 10 and posx <=11 and posy>=0 and posy < 8):
+				if not (posx,posy) in positions:
+					log.info("ChoixPos: append de (%d,%d)",posx,posy)
+					k+=1
+					positions.append((posx,posy))
+				else:
+					log.info("ChoixPos: position déjà occupée")
+			else:
+				log.info("ChoixPos : (%d,%d) hors limite",posx,posy)
+
+	return positions
 
 class jeu :
 	def __init__(self):
-		self.matrice = [ [0 for i in range(nbColonne)] for j in nbLigne]
-		self.equipe1 = 
-		self.equipe2
-		self.ballon
+		log.debug("Initialisation du jeu")
+		self.matrice = [ [0 for i in range(nbLigne)] for j in range(nbColonne)]
+		print("Choix équipe 1")
+		positions1 = choixPos(1)
+		print("Choix équipe 2")
+		positions2 = choixPos(2)
+		self.equipe1 = equipe(self,1,positions1)
+		self.equipe2 = equipe(self,2,positions2)
+		self.ballon = ballon((7,1+random.randint(1,6)))
+		self.tour = 1
 
 class ballon :
-	def __init__(self):
-		self.position
+	def __init__(self,position):
+		self.position = position
 
 class joueur : 
-	def __init__(self,jeu,nbEquipe,position,classe ):
+	def __init__(self,equipe,jeu,nEquipe,position,prop):
 		self.position = position
-		self.classe = classe
 		self.porteur = False
 		self.jeu = jeu
 		self.ko = False
-		self.equipe 
+		self.equipe = equipe
+		self.prop = prop
+		jeu.matrice[position[0]][position[1]] = self 
 
 class equipe :
-	def __init__(self,jeu,nbEquipe,position,classe):
-		self.equipe = [joueur(jeu,nbEquipe,position[i],classe[i]) for i in range(6)]
+	def __init__(self,jeu,nEquipe,positions):
+		self.equipe = [joueur(self,jeu,nEquipe,positions[i],prop[i]) for i in range(6)]
 		self.score = 0
 		self.jeu = jeu
 		self.carte = [True for i in range(6)]
 
+jeu = jeu()
+print(jeu.matrice[5][2])
